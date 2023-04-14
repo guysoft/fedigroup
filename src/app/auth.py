09e0812399d6fa985bc000be78afff64f37d1f06
Guy Sheffer <guysoft@gmail.com>
2023-04-14 22:14:51 +0000

    Add missing auth module

---
+++ src/app/auth.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+from app.crud import get_settings_secret
+
+"""
+By default, the CRSF cookies will be called csrf_access_token and
+csrf_refresh_token, and in protected endpoints we will look
+for the CSRF token in the 'X-CSRF-Token' headers. only certain
+methods should define CSRF token in headers default is ('POST','PUT','PATCH','DELETE')
+"""
+
+class User(BaseModel):
+    username: str
+    password: str
+
+class Settings(BaseModel):
+    authjwt_secret_key: str = "secret"
+    # Configure application to store and get JWT from cookies
+    authjwt_token_location: set = {"cookies"}
+    # Only allow JWT cookies to be sent over https
+    authjwt_cookie_secure: bool = True
+    # Enable csrf double submit protection. default is True
+    authjwt_cookie_csrf_protect: bool = False
+    # Change to 'lax' in production to make your website more secure from CSRF Attacks, default is None
+    authjwt_cookie_samesite: str = 'lax'
+
+
