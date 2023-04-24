de4e48207d65a8878f7378dd97187e879b0d6e0d
Guy Sheffer <guysoft@gmail.com>
2023-04-24 19:27:41 +0000

    Re-enable authjwt_cookie_csrf_protect

--- src/app/auth.py
+++ src/app/auth.py
@@ -19,7 +19,7 @@ class Settings(BaseModel):
     # Only allow JWT cookies to be sent over https
     authjwt_cookie_secure: bool = True
     # Enable csrf double submit protection. default is True
-    authjwt_cookie_csrf_protect: bool = False
+    authjwt_cookie_csrf_protect: bool = True
     # Change to 'lax' in production to make your website more secure from CSRF Attacks, default is None
     authjwt_cookie_samesite: str = 'lax'
 
