a8e4df6068a6b2d887d821f8ff96857e458a4bac
Guy Sheffer <guysoft@gmail.com>
2023-05-31 20:30:56 +0000

    Remove async calls

--- src/app/fgctl.py
+++ src/app/fgctl.py
@@ -9,45 +9,38 @@ from app.common import SERVER_URL, multi_urljoin
 app = typer.Typer(pretty_exceptions_enable=False)
 
 
-from app.db import SessionLocal, database
+from app.db import SessionLocal
 
 @app.command()
 def group_message(group: str, message: str):
     print(group, message)
-
     db = SessionLocal()
-    database.connect()
 
     preshared_key_id = multi_urljoin(SERVER_URL, "group", group) + "#main-key"
     key_path = "/data/default_gpg_key/id_rsa"
 
     fedigroup_message(db, group, message, preshared_key_id, key_path)
     db.close()
-    database.disconnect()
 
 @app.command()
 def boost_message(group: str, note_id: str):
     print(group, note_id)
 
     db = SessionLocal()
-    database.connect()
 
     preshared_key_id = multi_urljoin(SERVER_URL, "group", group) + "#main-key"
     key_path = "/data/default_gpg_key/id_rsa"
 
     fedigroup_boost(db, group, note_id, preshared_key_id, key_path)
     db.close()
-    database.disconnect()
 
 
 @app.command()
 def post_of_member(actor_handle: str):
     db = SessionLocal()
-    database.connect()
     for post in get_posts_for_member(db, actor_handle):
         print(f'{post.content} at {post.created_at}')
     db.close()
-    database.disconnect()
 
 
 
